be86909114cf78c9a6675ace9c4e712b2a34e239
Roberto Bonvallet <rbonvall@gmail.com>
2010-02-19 00:52:21 -0300

    Some tests

---
+++ tests.py
@@ -0,0 +1,40 @@
+from carioca import *
+import unittest
+
+class Trios(unittest.TestCase):
+    def test_trio_different_suites(self):
+        cards = [Card(A, SPADES), Card(A, HEARTS), Card(A, DIAMONDS)]
+        self.assertTrue(is_trio(cards))
+    def test_trio_one_repeated_suit(self):
+        cards = [Card(7, CLUBS), Card(7, SPADES), Card(7, CLUBS)]
+        self.assertTrue(is_trio(cards))
+    def test_just_one_suit(self):
+        cards = [Card(K, HEARTS), Card(K, HEARTS), Card(K, HEARTS)]
+        self.assertTrue(is_trio(cards))
+    def test_joker_at_beginning(self):
+        cards = [Card(JOKER, None), Card(10, SPADES), Card(10, DIAMONDS)]
+        self.assertTrue(is_trio(cards))
+    def test_joker_in_the_middle(self):
+        cards = [Card(2, DIAMONDS), Card(JOKER, None), Card(2, CLUBS)]
+        self.assertTrue(is_trio(cards))
+
+
+class Decks(unittest.TestCase):
+    def setUp(self):
+        self.deck = create_deck()
+
+    def test_deck_size(self):
+        self.assertEqual(len(self.deck), 54)
+    def test_nr_jokers(self):
+        jokers = [card for card in self.deck if card.rank == JOKER]
+        self.assertEqual(len(jokers), 2)
+    def test_nr_ranks(self):
+        ranks = set(card.rank for card in self.deck)
+        self.assertEqual(len(ranks), 14)
+    def test_all_suites_present(self):
+        suites = set(card.suit for card in self.deck if card.suit)
+        self.assertEqual(len(suites), 4)
+
+
+if __name__ == "__main__":
+    unittest.main()
